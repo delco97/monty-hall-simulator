bd99369a0b09b31b09e94ddb574474b3b97b140e
Andrea Del Corto <andrea.delcorto@gmail.com>
2025-01-23 14:45:19 +0100

    bug fixing

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import random
 
 def monty_hall_simulation(num_simulations: int, num_doors: int = 3):
     stay_wins = 0
+    stay_loses = 0
     switch_wins = 0
+    switch_loses = 0
 
     for _ in range(num_simulations):
         # Si posiziona il premio dietro una delle tre porte
         doors = [0] * num_doors
-        prize_door = random.randint(0, 2)
+        prize_door = random.randint(0, len(doors) - 1)
         doors[prize_door] = 1
 
         # Il giocatore sceglie una porta a caso
-        player_choice = random.randint(0, 2)
+        player_choice = random.randint(0, len(doors) - 1)
 
         # Il conduttore apre una porta vuota
         loosing_doors = [
@@ -20,7 +22,7 @@ def monty_hall_simulation(num_simulations: int, num_doors: int = 3):
         ]
         monty_opens = random.choice(loosing_doors)
 
-        # Se giocatore decide di cambiare, ne scegli una tra le porte rimanenti
+        # Se giocatore decide di cambiare, ne sceglie una tra le porte rimanenti
         remaining_doors = [
             i for i in range(len(doors))
             if i != player_choice and i != monty_opens
@@ -29,13 +31,19 @@ def monty_hall_simulation(num_simulations: int, num_doors: int = 3):
 
         # Verificare il risultato
         if doors[player_choice] == 1:
-            stay_wins += 1  # Vincita mantenendo la scelta iniziale
-        elif doors[switch_door] == 1:
-            switch_wins += 1  # Vincita cambiando scelta
+            stay_wins += 1
+        else:
+            stay_loses += 1
+        if doors[switch_door] == 1:
+            switch_wins += 1
+        else:
+            switch_loses += 1
 
     # Risultati finali
-    print(f"Numero di simulazioni: {num_simulations}")
-    print(f"Vittorie mantenendo la scelta iniziale: {stay_wins} ({stay_wins / num_simulations:.2%})")
-    print(f"Vittorie cambiando scelta: {switch_wins} ({switch_wins / num_simulations:.2%})")
+    print(f"Numero di simulazioni: {num_simulations}\n")
+    print(f"Vittorie mantenendo la scelta iniziale: {stay_wins} ({(stay_wins / num_simulations) * 100:.2f}%)")
+    print(f"Sconfitte mantenendo la scelta iniziale: {stay_loses} ({(stay_loses / num_simulations) * 100:.2f}%)\n")
+    print(f"Vittorie cambiando la scelta iniziale: {switch_wins} ({(switch_wins / num_simulations) * 100:.2f}%)")
+    print(f"Sconfitte cambiando la scelta iniziale: {switch_loses} ({(switch_loses / num_simulations) * 100:.2f}%)")
 
-monty_hall_simulation(10000, 4)
+monty_hall_simulation(100000, 4)
